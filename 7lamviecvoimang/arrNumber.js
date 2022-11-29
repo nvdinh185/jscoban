8e4c599b667e1e595097435f200d41ad9401dd47
nvdinh185 <nvdinh0766777123@gmail.com>
2022-11-29 17:43:45 +0700

    add

---
+++ 7lamviecvoimang/arrNumber.js
@@ -0,0 +1,41 @@
+var arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Xóa phần tử cuối cùng của mảng
+// console.log(arrNumber.pop());
+
+// Xóa phần tử đầu tiên của mảng
+// console.log(arrNumber.shift());
+
+// Thêm phần tử 0 vào cuối mảng
+// console.log(arrNumber.push(0));
+
+// Thêm phần tử 0 vào đầu mảng
+// console.log(arrNumber.unshift(0));
+
+// Cắt mảng từ phần tử đầu tiên đến phần tử thứ 5 thành 1 mảng mới
+// var arrNumberCut = arrNumber.splice(0, 5);
+
+// console.log(arrNumberCut);
+
+// Chèn thêm số 100 vào phần tử thứ 5 của mảng
+// var replaceArr = arrNumber.splice(4, 0, 100); // thực hiện chèn
+// console.log(replaceArr); // trả về một mảng rỗng
+
+// Thay thế phần tử thứ 5 bằng số 100
+// var replaceArr = arrNumber.splice(4, 1, 100); // thực hiện thay thế
+// console.log(replaceArr); // trả về phần tử thứ 5 (có chỉ số là 4)
+
+// copy từ mảng arrNumber thành 1 mảng mới từ phần tử thứ 5 đến hết
+// var copyArrNumber = arrNumber.slice(5);
+// console.log(copyArrNumber);
+
+// copy từ mảng arrNumber thành 1 mảng mới từ phần tử đầu tiên đến phần tử thứ 5
+// var copyArrNumber2 = arrNumber.slice(0, 5);
+// console.log(copyArrNumber2);
+
+var arrNumber2 = [11, 22, 33, 44, 55];
+
+// Nối mảng arrNumber2 vào mảng arrNumber
+// arrNumber = arrNumber.concat(arrNumber2);
+
+console.log(arrNumber);
---
+++ 9lenhrenhanh-toantu3ngoi/84lenhrenhanhifelse.js
@@ -0,0 +1,11 @@
+var day = 3;
+
+if (day == 2) {
+    console.log('Hôm nay là thứ hai');
+} else if (day == 3) {
+    console.log('Hôm nay là thứ ba');
+} else if (day == 4) {
+    console.log('Hôm nay là thứ tư');
+} else if (day == 5) {
+    console.log('Hôm nay là thứ năm');
+}
