c2ec447bf595fc41251367ea4ab3780822b2cce4
nvdinh185 <nvdinh0766777123@gmail.com>
2023-01-31 09:36:26 +0700

    rename folder

--- 2bien-comments-builtin/bai2.js
+++ 2bien-comments-builtin/bai2.js
@@ -10,11 +10,3 @@ var c = prompt("Nhập 1 số: ");
 
 console.log(c);
 console.log(typeof c);
-
-setTimeout(function () {
-    alert("123");
-}, 1000);
-
-setInterval(function () {
-    alert("456");
-}, 1000);
