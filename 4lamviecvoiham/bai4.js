8a5d6deeca28346c6aaad0e1664247e86a6b06e8
nvdinh185 <nvdinh0766777123@gmail.com>
2023-04-07 09:52:20 +0700

    x

--- 4lamviecvoiham/bai4.js
+++ 4lamviecvoiham/bai4.js
@@ -1,42 +1,42 @@
 // Hàm in dãy số đã nhập
 function writeLog() {
-    for (var i = 0; i < arguments.length; i++) {
-        console.log(arguments[i]);
+    for (var n of arguments) {
+        console.log(n);
     }
 }
+writeLog(1, 2, 3, 4, 5);
 
 // Hàm tính tổng của dãy số
 function sumArray() {
     var sum = 0;
-    for (var i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
+    for (var n of arguments) {
+        sum += n;
     }
     return sum;
 }
+console.log(sumArray(1, 2, 3, 4, 5, 6));
 
 // Hàm tìm số lớn nhất
 function findMax() {
     var max = arguments[0];
-    for (var i = 1; i < arguments.length; i++) {
-        if (arguments[i] > max) {
-            max = arguments[i];
+    for (var n of arguments) {
+        if (n > max) {
+            max = n;
         }
     }
     return max;
 }
+console.log(findMax(1, 123, 500, 115, 44, 88));
 
 // Hàm tìm số nhỏ nhất
 function findMin() {
     var min = arguments[0];
-    for (var i = 1; i < arguments.length; i++) {
-        if (arguments[i] < min) {
-            min = arguments[i];
+    for (var n of arguments) {
+        if (n < min) {
+            min = n;
         }
     }
     return min;
 }
 
-writeLog(1, 2, 3, 4, 5);
-console.log(sumArray(1, 2, 3, 4, 5, 6));
-console.log(findMax(1, 123, 500, 115, 44, 88));
 console.log(findMin(100, 123, 500, 115, 44, 88));
