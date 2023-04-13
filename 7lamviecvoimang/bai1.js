50e613852ebfb8e976728a69b1c0439c7b1f1a86
nvdinh185 <nvdinh0766777123@gmail.com>
2023-04-13 10:28:35 +0700

    x

--- 7lamviecvoimang/bai1.js
+++ 7lamviecvoimang/bai1.js
@@ -1,15 +1,15 @@
 // Hàm nhập mảng các số từ bàn phím
 function nhapMang(array, n) {
     for (var i = 1; i <= n; i++) {
-        var n = prompt('Nhập phần tử thứ ' + i);
-        array.push(n);
+        var el = prompt('Nhập phần tử thứ ' + i);
+        array.push(el);
     }
 }
 
 // Hàm xuất mảng ra màn hình console
 function xuatMang(array) {
-    for (var el of array) {
-        console.log(el);
+    for (var i = 0; i < n; i++) {
+        console.log(array[i]);
     }
 }
 
