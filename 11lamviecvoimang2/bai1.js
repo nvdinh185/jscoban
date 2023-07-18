1762156cf80619ae2d8e15b9668c76b84d08a5b2
nvdinh185 <nvdinh0766777123@gmail.com>
2023-07-18 08:12:34 +0700

    x

--- 11lamviecvoimang2/bai1.js
+++ 11lamviecvoimang2/bai1.js
@@ -1,59 +1,66 @@
-var students = [
-    {
-        id: 1,
-        name: "Dinh",
-        address: "hue"
-    },
-    {
-        id: 2,
-        name: "Nam",
-        address: "quang nam"
-    },
-    {
-        id: 3,
-        name: "Tan",
-        address: "da nang"
-    },
-    {
-        id: 4,
-        name: "Hung",
-        address: "hue"
-    },
-    {
-        id: 5,
-        name: "Tri",
-        address: "quang tri"
-    },
-    {
-        id: 6,
-        name: "Anh",
-        address: "hue"
-    },
-    {
-        id: 7,
-        name: "Binh",
-        address: "da nang"
+// Hàm nhập mảng các số từ bàn phím
+function nhapMang(array, n) {
+    for (var i = 1; i <= n; i++) {
+        var el = prompt('Nhập phần tử thứ ' + i);
+        array.push(el);
     }
-];
-
-// Cách 1: Dùng for each
-students.forEach(function (student) {
-    student.toan = Math.floor(Math.random() * 11);
-    student.ly = Math.floor(Math.random() * 11);
-    student.hoa = Math.floor(Math.random() * 11);
-});
-
-// Cách 2: Dùng map
-// students = students.map(function (student) {
-//     student.toan = Math.floor(Math.random() * 11);
-//     student.ly = Math.floor(Math.random() * 11);
-//     student.hoa = Math.floor(Math.random() * 11);
-//     return student;
-// });
-console.log("=== DANH SÁCH SINH VIÊN ===");
-for (const el of students) {
-    for (const key in el) {
-        console.log(key + ": " + el[key]);
-    }
-    console.log('===================');
 }
+
+// Hàm kiểm tra xem thử có phải tất cả các phần tử đều lớn hơn 5 không? (dùng every)
+function checkEvery(array) {
+    return array.every(function (el) {
+        return el >= 5;
+    });
+}
+
+// Hàm kiểm tra xem thử có phần tử nào đều lớn hơn 5 không? (dùng some)
+function checkSome(array) {
+    return array.some(function (el) {
+        return el >= 5;
+    });
+}
+
+// Hàm nhân 3 giá trị mỗi phần tử trong mảng (dùng map)
+function triple(array) {
+    return array.map(function (el) {
+        return el *= 3;
+    });
+    // var newArr = [];
+    // for (const el of array) {
+    //     newArr.push(el * 3);
+    // }
+    // return newArr;
+}
+
+// tính tổng các phần tử trong mảng (dùng reduce)
+function sumArray(array) {
+    return array.reduce(function (total, el) {
+        return total + el;
+    }, 0);
+}
+
+// Hàm xuất mảng ra màn hình console
+function xuatMang(array) {
+    for (var i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+var arNum = [];
+// goi hàm nhập 1 mảng
+nhapMang(arNum, 2);
+
+// gọi hàm nhân 3 giá trị mỗi phần tử trong mảng
+arNum = triple(arNum);
+
+// gọi hàm xuất mảng
+xuatMang(arNum);
+
+// gọi hàm tính tổng các phần tử trong mảng
+console.log(sumArray(arNum));
+
+// gọi hàm kiểm tra xem thử có phải tất cả các phần tử đều lớn hơn 5 không?
+console.log(checkEvery(arNum));
+
+// gọi hàm kiểm tra xem thử có phần tử nào đều lớn hơn 5 không?
+console.log(checkSome(arNum));
--- 11lamviecvoimang2/bai4.js
+++ 11lamviecvoimang2/bai4.js
@@ -1,66 +1,63 @@
-// Hàm nhập mảng các số từ bàn phím
-function nhapMang(array, n) {
-    for (var i = 1; i <= n; i++) {
-        var el = prompt('Nhập phần tử thứ ' + i);
-        array.push(el);
+const students = [
+    {
+        id: 1,
+        name: 'Nguyen Van Teo',
+        classId: 1
+    },
+    {
+        id: 2,
+        name: 'Nguyen Van Ti',
+        classId: 2
+    },
+    {
+        id: 3,
+        name: 'Tran Van Tun',
+        classId: 3
+    },
+    {
+        id: 4,
+        name: 'Nguyen Thi Heo',
+        classId: 1
+    },
+    {
+        id: 5,
+        name: 'Le Thi Be',
+        classId: 1
     }
-}
+]
 
-// Hàm kiểm tra xem thử có phải tất cả các phần tử đều lớn hơn 5 không? (dùng every)
-function checkEvery(array) {
-    return array.every(function (el) {
-        return el >= 5;
-    });
-}
-
-// Hàm kiểm tra xem thử có phần tử nào đều lớn hơn 5 không? (dùng some)
-function checkSome(array) {
-    return array.some(function (el) {
-        return el >= 5;
-    });
-}
-
-// Hàm nhân 3 giá trị mỗi phần tử trong mảng (dùng map)
-function triple(array) {
-    return array.map(function (el) {
-        return el *= 3;
-    });
-    // var newArr = [];
-    // for (const el of array) {
-    //     newArr.push(el * 3);
-    // }
-    // return newArr;
-}
-
-// tính tổng các phần tử trong mảng (dùng reduce)
-function sumArray(array) {
-    return array.reduce(function (total, el) {
-        return total + el;
-    }, 0);
-}
-
-// Hàm xuất mảng ra màn hình console
-function xuatMang(array) {
-    for (var i = 0; i < array.length; i++) {
-        console.log(array[i]);
+const classList = [
+    {
+        id: 1,
+        name: "CNTT"
+    },
+    {
+        id: 2,
+        name: 'DTVT'
+    },
+    {
+        id: 3,
+        name: 'THXD'
+    },
+    {
+        id: 4,
+        name: 'XDDD'
     }
-}
+]
 
-var arNum = [];
-// goi hàm nhập 1 mảng
-nhapMang(arNum, 2);
+const ITStudents = [];
+students.forEach(function (student) {
+    const classInfo = classList.find(function (el) {
+        return el.id === student.classId;
+    })
+    if (classInfo.id === 1) {
+        const it = {
+            id: student.id,
+            studentName: student.name,
+            className: classInfo.name
+        }
+        ITStudents.push(it);
+    }
+})
 
-// gọi hàm nhân 3 giá trị mỗi phần tử trong mảng
-arNum = triple(arNum);
-
-// gọi hàm xuất mảng
-xuatMang(arNum);
-
-// gọi hàm tính tổng các phần tử trong mảng
-console.log(sumArray(arNum));
-
-// gọi hàm kiểm tra xem thử có phải tất cả các phần tử đều lớn hơn 5 không?
-console.log(checkEvery(arNum));
-
-// gọi hàm kiểm tra xem thử có phần tử nào đều lớn hơn 5 không?
-console.log(checkSome(arNum));
+console.table(ITStudents);
